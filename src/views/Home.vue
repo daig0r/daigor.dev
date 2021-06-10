00b036d5bf6ecaac472dbfa64597cd07c08e437f
Diego Rodriguez <daig0r@pm.me>
2021-06-09 20:54:27 -0500

    Init Homer's webpage GIF

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <center>
+      <img
+        class="responsive"
+        src="../assets/homerswebpage.gif"
+        alt="homerswebpage"
+      />
+    </center>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+.responsive {
+  width: 100%;
+  height: auto;
+}
+</style>
